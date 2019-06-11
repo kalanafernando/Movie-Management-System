055e5cf4743dff642357c1e09aca18e6953cfbe4
kalana <kalanafdo96@gmail.com>
2019-06-11 05:25:04 -0700

    movie management system

---
+++ README.md
@@ -0,0 +1,23 @@
+#Movie Management System
+
+##Technologies
+JavaEE
+
+Java FX
+
+Spring
+
+Git
+
+GitHub
+
+Maven
+
+Spring Data JPA
+
+
+
+
+
+
+
---
+++ src/main/java/lk/ijse/dep/controller/DashboardController.java
@@ -0,0 +1,55 @@
+package lk.ijse.dep.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController  implements Initializable {
+
+    public ImageView imgRegisterActors;
+    @FXML
+    private ImageView imgManageMovie;
+
+
+    @FXML
+    void imgManageActors_OnMouseClicked(MouseEvent event) throws IOException {
+        Parent manageActor = FXMLLoader.load(this.getClass().getResource("/fxml/Manageactors.fxml"));
+        Scene firstScene = new Scene(manageActor);
+
+        Stage mainStage = (Stage) imgManageMovie.getScene().getWindow();
+        mainStage.setScene(firstScene);
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void imgRegisterActors_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        Parent manageActor = FXMLLoader.load(this.getClass().getResource("/fxml/RegisterActors.fxml"));
+        Scene firstScene = new Scene(manageActor);
+
+        Stage mainStage = (Stage) imgManageMovie.getScene().getWindow();
+        mainStage.setScene(firstScene);
+    }
+
+    public void imgManageMovie_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        Parent manageActor = FXMLLoader.load(this.getClass().getResource("/fxml/ManageMovies.fxml"));
+        Scene firstScene = new Scene(manageActor);
+
+        Stage mainStage = (Stage) imgManageMovie.getScene().getWindow();
+        mainStage.setScene(firstScene);
+    }
+}
